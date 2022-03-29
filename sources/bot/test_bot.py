092984408af090449ddb77af849bb9d47ad9f423
dany.small <dany.small.work@gmail.com>
2022-03-29 13:45:15 +0300

    Another test #10

--- sources/bot/test_bot.py
+++ sources/bot/test_bot.py
@@ -3,21 +3,25 @@ from telethon import TelegramClient, events
 import asyncio
 
 
-async def main():
-    """."""
-    API_ID = 7212719
-    API_HASH = '3778859a51ffe2951f3abe886d03d0f1'
-    BOT_TOKEN = '5238448407:AAFC6Y2qesRNmXgbzTQ3KbvRpLaHR24jgmE'
-    BOT_SESSION_NAME = 'bot-session'
+API_ID = 7212719
+API_HASH = '3778859a51ffe2951f3abe886d03d0f1'
+BOT_TOKEN = '5238448407:AAFC6Y2qesRNmXgbzTQ3KbvRpLaHR24jgmE'
+BOT_SESSION_NAME = 'bot-session'
 
-    client = TelegramClient(BOT_SESSION_NAME, API_ID, API_HASH)
-    client.start(bot_token=BOT_TOKEN)
+class Test():
 
-    @client.on(events.NewMessage)
-    async def handler(event):
-        event.reply(event)
+    def __init__(self):
+        self.client = TelegramClient(BOT_SESSION_NAME, API_ID, API_HASH)
 
-    client.run_until_disconnected()
+    async def _run(self):
+        await self.client.start(bot_token=BOT_TOKEN)
+        print('bot started')
+        await self.client.run_until_disconnected()
+
+    def run(self):
+        asyncio.run(self._run())
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    t = Test()
+    t.run()
